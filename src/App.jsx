438b3e054fa83f82868daaec6b73d7548d677a36
sine <sinemistermekonen@gmail.com>
2026-09-02 23:16:38 +0300

    day27 Addis Eats menu

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Menu from "./Menu";
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const dishes = [
+  {
+    id: 1,
+    name: "Doro Wat",
+    price: 350,
+    category: "Ethiopian",
+    spicy: true
+  },
+  {
+    id: 2,
+    name: "Tibs",
+    price: 400,
+    category: "Ethiopian",
+    spicy: true
+  },
+  {
+    id: 3,
+    name: "Shiro",
+    price: 250,
+    category: "Ethiopian",
+    spicy: false
+  },
+  {
+    id: 4,
+    name: "Burger",
+    price: 300,
+    category: "Fast Food",
+    spicy: false
+  },
+  {
+    id: 5,
+    name: "Pizza",
+    price: 450,
+    category: "Fast Food",
+    spicy: false
+  },
+  {
+    id: 6,
+    name: "Pasta",
+    price: 350,
+    category: "Italian",
+    spicy: false
+  }
+];
